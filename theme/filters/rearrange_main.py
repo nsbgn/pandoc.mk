92c4932c5593f3d35e2aedd0b8181e06ca47f251
nsv <nsv@valk.ws>
2017-06-26 02:11:06 +0200

    Reference section is now a variable in template.
    The filter only handles extraction of the reference section.
    
    Wrapping the document in <main> tags is now also done in the template
    rather than through a filter.

--- theme/filters/rearrange_main.py
+++ theme/filters/rearrange_main.py
@@ -1,104 +1,28 @@
 #!/usr/bin/env python3
 
 """
-This filter wraps the document in <main> tags with footers and headers, and
-relocates the reference section.
+This filter puts references in the metadata, so that they can be
+relocated in the template.
 """
 
+# TODO: Do the same for footnotes, somehow
+
 import panflute as pf
-from datetime import datetime
 
-
-def meta(doc, key):
-    """
-    Get string value of document's metadata.
-    """
-
-    value = doc.get_metadata(key)
-    return pf.stringify(value) if value else ""
-
-
-
-def format_date(date):
-    """
-    Transform string date from 2000-01-01 format into 1 January 2000 format,
-    if possible.
-    """
-    
-    try:
-        date = datetime.strptime(date, "%Y-%m-%d").strftime("%-d %B %Y")
-    except (ValueError, TypeError):
-        pass
-    return date
-
-
-
-def wrap_main(doc):
-    """
-    Wrap the document in <main> tags, decorated with metadata in a footer.
-    """
-    
-    abstract = meta(doc, "abstract") or meta(doc, "description")
-    if abstract:
-        abstractDiv = pf.Div(pf.Plain(pf.Str(abstract)), classes=["abstract"])
-    else:
-        abstractDiv = pf.Null()
-
-
-    doc.content = ([
-        pf.RawBlock('<main>'),
-        abstractDiv,
-    ] + list(doc.content) + [
-        pf.RawBlock('<footer>&mdash; {}</footer></main>'.format(
-            ",".join(filter(None, [
-                meta(doc, "author") or "Anonymous",
-                format_date(meta(doc, "date"))
-            ]))
-        ))
-    ])
-
-
-
-    
-def extract_refs(el, doc):
-    """
-    Lift the <div> with references out of the document so that it can be 
-    reinserted as a <section> later on.
-    """
-    
+def prepare(doc):
+    doc.references = None
+     
+def action(el, doc):
     if isinstance(el, pf.Div) and el.identifier == "refs":
         doc.references = list(el.content)
         return []
 
-
-
-def insert_refs(doc):
-    """
-    Re-insert the extracted references as a <section> tag.
-    """
-    if doc.references:
-        doc.content = (
-            list(doc.content) +
-            [ pf.RawBlock('<section class="references">') ] +
-            list(doc.references) +
-            [ pf.RawBlock("</section>") ]
-        )
-
-
-
-def prepare(doc):
-    doc.references = []
-    
-    
-def action(el, doc):
-    return extract_refs(el, doc)
-
-
 def finalize(doc):
-    wrap_main(doc)
-    insert_refs(doc)
+    if doc.references:
+        doc.metadata["references"] = pf.MetaBlocks(*doc.references) 
     del doc.references
-
 
 if __name__ == "__main__":
     pf.toJSONFilter(action, prepare, finalize)
+
+   
