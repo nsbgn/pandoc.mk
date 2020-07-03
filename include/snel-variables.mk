5c4e1dd48c4b817c135b24a745eec6927cf83333
nsv <nsv@valk.ws>
2020-07-03 16:26:18 +0200

    Put variables in seperate makefile module.

---
+++ include/snel-variables.mk
@@ -0,0 +1,41 @@
+# Location of this makefile
+BASE_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+
+# Installation directories
+PREFIX := /usr/local
+INCLUDE_DIR := $(PREFIX)/include
+SHARE_DIR := $(PREFIX)/share/snel
+
+# If installed in $PREFIX/include, we find other assets in $PREFIX/share/snel.
+# Otherwise, we find them relative to the current makefile
+ifeq ($(BASE_DIR),$(INCLUDE_DIR))
+    ASSET_DIR := $(SHARE_DIR)
+    JQ_DIR := $(SHARE_DIR)
+    PANDOC_DIR := $(SHARE_DIR)/pandoc
+else
+    ASSET_DIR := $(BASE_DIR)/../share
+    JQ_DIR := $(BASE_DIR)/../share
+    PANDOC_DIR := $(BASE_DIR)/../share/pandoc
+endif
+
+# Source and destination directories, and FTP credentials. These are
+# expected to be changed in the `make` call or before the `include` statement.
+ifndef SRC
+    SRC := .
+endif
+ifndef DEST
+    DEST := build
+endif
+ifndef STYLE
+    STYLE := letter
+endif
+ifndef CACHE
+    CACHE := $(DEST)/cache
+endif
+ifndef IGNORE
+    IGNORE=Makefile .git .gitignore
+endif
+ifndef HIDE_WEB_INFO
+	HIDE_WEB_INFO=
+endif
+
