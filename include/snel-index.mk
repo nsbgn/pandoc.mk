4f33e37d33cdc98c3f638eb05c63d01f30ed8f1b
nsv <nsv@valk.ws>
2020-07-10 16:11:00 +0200

    Add more feedback during recipes.

--- include/snel-index.mk
+++ include/snel-index.mk
@@ -12,28 +12,12 @@ SOURCE_FILES = $(shell \
 # Headers and extra targets are collected for each source in a corresponding file
 META_FILES = $(patsubst $(SRC)/%,$(CACHE)/%.meta.json,$(SOURCE_FILES))
 
-html: $(CACHE)/content.mk $(DEST)/favicon.ico $(DEST)/apple-touch-icon.png $(DEST)/index.html $(DEST)/web.css | external-targets html-targets
-pdf:  $(CACHE)/content.mk | external-targets pdf-targets
+html: $(CACHE)/dynamic.mk $(DEST)/favicon.ico $(DEST)/apple-touch-icon.png $(DEST)/index.html $(DEST)/web.css | external-targets html-targets
+pdf:  $(CACHE)/dynamic.mk | external-targets pdf-targets
 
-include $(CACHE)/content.mk
+include $(CACHE)/dynamic.mk
 
-# Generate the makefile containing the dynamic targets for HTML/PDF documents
-# and any external content referred inside
-$(CACHE)/content.mk: $(CACHE)/index.json
-	@echo "Generating content recipes..."
-	@jq -L"$(JQ_DIR)" --arg dest "$(DEST)" -r 'include "index"; targets($$dest) | to_entries | .[] | (.key + ": " + (.value | join(" ")))' < $< > $@
-	@echo ".PHONY: external-targets html-targets pdf-targets" >> "$@"
-
-# Optionally, remove all files in $(DEST) that are no longer targeted
-clean: $(CACHE)/targets.txt
-	@bash -i -c 'read -p "Operation might remove files in \"$(DEST)\". Continue? [y/N]" -n 1 -r; \
-	    [[ $$REPLY =~ ^[Yy]$$ ]] || exit 1'
-	@echo
-	find "$(DEST)" -type f -a -not -path '$(CACHE)/*' \
-	    | grep --fixed-strings --line-regexp --invert-match --file=$< \
-	    | xargs --no-run-if-empty rm --verbose
-
-.PHONY: all html pdf clean
+.PHONY: html pdf clean
 
 # Record metadata headers for each document
 $(CACHE)/%.md.headers.json: $(SRC)/%.md $(PANDOC_DIR)/metadata.json $(JQ_DIR)/index.jq
@@ -63,21 +47,37 @@ $(CACHE)/%.md.meta.json: $(CACHE)/%.md.headers.json $(CACHE)/%.md.targets.json
 	    $^ \
 	    > $@
 
+# Overview of files & directories, without metadata
+$(CACHE)/filetree.json: $(SOURCE_FILES)
+	@-mkdir -p $(@D)
+	@echo "Generating file tree \"$@\"..." 1>&2
+	@tree -JDpi --du --timefmt '%s' --dirsfirst \
+	    -I '$(subst $() $(),|,$(IGNORE))' \
+	    | jq '.[0]' \
+	    > $@
+
 # Overview of files & directories with metadata, readable for index template
 $(CACHE)/index.json: $(JQ_DIR)/index.jq \
 	    $(CACHE)/filetree.json \
 	    $(META_FILES) \
 	    $(wildcard $(SRC)/index.base.json)
 	@-mkdir -p $(@D)
-	@echo "Generating index data..." 1>&2
+	@echo "Generating index data \"$@\"..." 1>&2
 	@jq  -L$(JQ_DIR) --slurp \
 	    'include "index"; index' \
 	    $(filter %.json, $^) \
 	    > $@
 
+# Generate the makefile containing the dynamic targets for HTML/PDF documents
+# and any external content referred inside
+$(CACHE)/dynamic.mk: $(CACHE)/index.json
+	@echo "Generating Makefile for content \"$@\"..."
+	@jq -L"$(JQ_DIR)" --arg dest "$(DEST)" -r 'include "index"; targets($$dest) | to_entries | .[] | (.key + ": " + (.value | join(" ")))' < $< > $@
+	@echo ".PHONY: external-targets html-targets pdf-targets" >> "$@"
+
 # Overview of final targets
 $(CACHE)/targets.txt: $(CACHE)/index.json
-	@echo "Aggregating targets..." 1>&2
+	@echo "Aggregating targets \"$@\"..." 1>&2
 	@jq \
 	    -L"$(JQ_DIR)" \
 	    --arg dest "$(DEST)" \
@@ -85,19 +85,19 @@ $(CACHE)/targets.txt: $(CACHE)/index.json
 	    < $< \
 	    > $@
 
-# Overview of files & directories, without metadata
-$(CACHE)/filetree.json: $(SOURCE_FILES)
-	@-mkdir -p $(@D)
-	@echo "Generating file tree..." 1>&2
-	@tree -JDpi --du --timefmt '%s' --dirsfirst \
-	    -I '$(subst $() $(),|,$(IGNORE))' \
-	    | jq '.[0]' \
-	    > $@
+# Optionally, remove all files in $(DEST) that are no longer targeted
+clean: $(CACHE)/targets.txt
+	@bash -i -c 'read -p "Operation might remove files in \"$(DEST)\". Continue? [y/N]" -n 1 -r; \
+	    [[ $$REPLY =~ ^[Yy]$$ ]] || exit 1'
+	@echo
+	find "$(DEST)" -type f -a -not -path '$(CACHE)/*' \
+	    | grep --fixed-strings --line-regexp --invert-match --file=$< \
+	    | xargs --no-run-if-empty rm --verbose
 
 # Generate static index page 
 $(DEST)/index.html: $(PANDOC_DIR)/page.html $(PANDOC_DIR)/nav.html $(CACHE)/index.json
 	@-mkdir -p $(@D)
-	@echo "Generating index page..." 1>&2
+	@echo "Generating index page \"$@\"..." 1>&2
 	@echo | pandoc \
 	    --template="$(PANDOC_DIR)/page.html" \
 	    --metadata-file "$(CACHE)/index.json" \
