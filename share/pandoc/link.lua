86b3f16967dd448030622f91cd5f770e40b8b55f
nsv <nsv@valk.ws>
2020-09-03 13:16:47 +0200

    Control over URL substitution for `link.lua`.
    Gives user control over automatic URL insertion by adding codes. For
    example, the code `%d` in the link content will be converted to its
    target domain.
    
    Try with the following codes:
    
    -   [](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [](mailto:someone@domain.com)
    -   [%t](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%h](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%P](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%p](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%h%p](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%q](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%f](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%a](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%d](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [keep %d](https://sub.domain.com:80/page/file.ext?a=1&b=2#anchor)
    -   [%d](http://localhost)
    -   [%d](sub.domain.com)

--- share/pandoc/link.lua
+++ share/pandoc/link.lua
@@ -1,21 +1,62 @@
--- When this filter encounters a link with empty content, the content should be
--- set to the host of the target link, e.g.:
--- `[](https://link.com/page)` -> [`link.com`](https://link.com/page)
+-- When this filter encounters a link with empty content, the content is set to
+-- the target URL. Alternatively, if it consists ONLY of single characters
+-- prefixed with a `%`, then it should be set to the corresponding URL fragment
+-- (see the `dissect` function for relevant characters).
+--
+-- Example:
+--    [%d](https://sub.domain.com:80/page/file.ext?arg1=1&arg2=2#anchor) 
+-- →  [`domain.com`](https://sub.domain.com:80/page/file.ext?arg1=1&arg2=2#anchor)
 
-function host(str)
-    str = str:gsub("^%a+://","")
-    str = str:gsub("www.","")
-    str = str:gsub("mailto:","")
-    str = str:gsub("/[^/]+","")
-    str = str:gsub("/$","")
-    return str
+function take(pattern, str)
+    local out = ""
+    local str = str
+    local i, j = str:find(pattern)
+    if i and j then
+        out = str:sub(i,j)
+        str = str:sub(1,i-1)..str:sub(j+1)
+    end
+    return out, str
+end
+
+function dissect(url)
+    protocol, url = take("^%a+://", url)
+    host, url = take("^[^:/]+", url)
+    port, url = take("^:[^/]+", url)
+    path, url = take("^/[^%?]*", url)
+    query, url = take("^?[^#]+", url)
+    anchor, url = take("^#.+", url)
+    file = path:match("[^/]+$") or ""
+    domain = host:match("[^%.]+%.[^%.]+$") or host
+    
+    return {
+        t = protocol, -- "https://"
+        h = host, -- "sub.domain.com"
+        d = domain, -- "domain.com"
+        P = port, -- ":80"
+        p = path, -- "/directory/file.ext"
+        f = file, -- "file.ext"
+        q = query, -- "?param1=true&param2=string"
+        a = anchor, -- "#hash-fragment"
+    }
+end
+
+function subst(string, substitution)
+    for k,v in pairs(substitution) do
+        string = string:gsub("%%"..k, v)
+    end
+    return string
 end
 
 return {
     {
         Link = function (a)
             if #a.content == 0 then
-                a.content = { pandoc.Code(host(a.target)) }
+                url = a.target:gsub("^mailto:", "")
+                a.content = { pandoc.Code(url) }
+            elseif #a.content == 1
+            and a.content[1].t == "Str"
+            and a.content[1].text:gsub("%%%a","") == "" then
+                a.content = { pandoc.Code(subst(a.content[1].text, dissect(a.target))) }
             end
             return a
         end,
