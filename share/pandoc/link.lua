3214f4a005bcc6d7b94989b99609f4ae71f06c22
nsv <nsv@valk.ws>
2020-09-04 13:22:34 +0200

    Follow style guide for `lua` files.
    Follow the style guide in `github.com/Olivine-Labs/lua-style-guide` as
    recommended by `github.com/pandoc/lua-filters`.

--- share/pandoc/link.lua
+++ share/pandoc/link.lua
@@ -1,3 +1,4 @@
+-- DRY-links.
 -- When this filter encounters a link with empty content, the content is set to
 -- the target URL. Alternatively, if it consists ONLY of single characters
 -- prefixed with a `%`, then it should be set to the corresponding URL fragment
@@ -7,58 +8,59 @@
 --    [%d](https://sub.domain.com:80/page/file.ext?arg1=1&arg2=2#anchor) 
 -- →  [`domain.com`](https://sub.domain.com:80/page/file.ext?arg1=1&arg2=2#anchor)
 
-function take(pattern, str)
-    local out = ""
-    local str = str
-    local i, j = str:find(pattern)
-    if i and j then
-        out = str:sub(i,j)
-        str = str:sub(1,i-1)..str:sub(j+1)
-    end
-    return out, str
+local function take(pattern, string)
+  local output = ''
+  local i, j = string:find(pattern)
+  if i and j then
+    output = string:sub(i, j)
+    string = string:sub(1, i-1) .. string:sub(j+1)
+  end
+  return output, string
 end
 
-function dissect(url)
-    protocol, url = take("^%a+://", url)
-    host, url = take("^[^:/]+", url)
-    port, url = take("^:[^/]+", url)
-    path, url = take("^/[^%?]*", url)
-    query, url = take("^?[^#]+", url)
-    anchor, url = take("^#.+", url)
-    file = path:match("[^/]+$") or ""
-    domain = host:match("[^%.]+%.[^%.]+$") or host
-    
-    return {
-        t = protocol, -- "https://"
-        h = host, -- "sub.domain.com"
-        d = domain, -- "domain.com"
-        P = port, -- ":80"
-        p = path, -- "/directory/file.ext"
-        f = file, -- "file.ext"
-        q = query, -- "?param1=true&param2=string"
-        a = anchor, -- "#hash-fragment"
-    }
+local function subst(string, substitution)
+  for k, v in pairs(substitution) do
+    string = string:gsub('%%' .. k, v)
+  end
+  return string
 end
 
-function subst(string, substitution)
-    for k,v in pairs(substitution) do
-        string = string:gsub("%%"..k, v)
-    end
-    return string
+local function dissect(url)
+  protocol, url = take('^%a+:/*', url)
+  host, url = take('^[^:/]+', url)
+  port, url = take('^:[^/]+', url)
+  path, url = take('^/[^%?]*', url)
+  query, url = take('^?[^#]+', url)
+  anchor, url = take('^#.+', url)
+  file = path:match('[^/]+$') or ''
+  domain = host:match('[^%.]+%.[^%.]+$') or host
+
+  return {
+    t = protocol, -- 'https://'
+    h = host,     -- 'sub.domain.com'
+    d = domain,   -- 'domain.com'
+    P = port,     -- ':80'
+    p = path,     -- '/directory/file.ext'
+    f = file,     -- 'file.ext'
+    q = query,    -- '?param1=true&param2=string'
+    a = anchor    -- '#hash-fragment'
+  }
 end
 
 return {
-    {
-        Link = function (a)
-            if #a.content == 0 then
-                url = a.target:gsub("^mailto:", "")
-                a.content = { pandoc.Code(url) }
-            elseif #a.content == 1
-            and a.content[1].t == "Str"
-            and a.content[1].text:gsub("%%%a","") == "" then
-                a.content = { pandoc.Code(subst(a.content[1].text, dissect(a.target))) }
-            end
-            return a
-        end,
-    }
+  {
+    Link = function(a)
+      if #a.content == 0 then
+        local url = a.target:gsub('^mailto:', '')
+        a.content = { pandoc.Code(url) }
+      elseif #a.content == 1 and a.content[1].t == 'Str'
+          and a.content[1].text:gsub('%%%a','') == '' then
+        a.content = {
+          pandoc.Code(subst(a.content[1].text, dissect(a.target)))
+        }
+      end
+      return a
+    end,
+  }
 }
+
--- share/pandoc/resources.lua
+++ share/pandoc/resources.lua
@@ -4,27 +4,28 @@
 
 local resources = {}
 
-function add_resource(url)
-    -- Test if URL is local & relative
-    if url:sub(1, 1) ~= '/'
-    and (url:match("^%a+://") or url:match("^mailto:")) == nil
-    then table.insert(resources, pandoc.MetaString(url))
-    end
+-- Add resource if URL is local & relative
+local function add_resource(url)
+  if url:sub(1, 1) ~= '/' and
+      (url:match("^%a+://") or url:match("^mailto:")) == nil then
+    table.insert(resources, pandoc.MetaString(url))
+  end
 end
 
 return {
-    {
-        Link = function (a)
-            add_resource(a.target)
-            return nil
-        end,
-        Image = function (img)
-            add_resource(img.src)
-            return nil
-        end,
-        Meta = function (meta)
-            meta.resources = pandoc.MetaList(resources)
-            return meta
-        end,
-    }
+  {
+    Link = function(a)
+      add_resource(a.target)
+      return nil
+    end,
+    Image = function(img)
+      add_resource(img.src)
+      return nil
+    end,
+    Meta = function(meta)
+      meta.resources = pandoc.MetaList(resources)
+      return meta
+    end,
+  }
 }
+
